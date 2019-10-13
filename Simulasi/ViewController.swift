72bf9407e4d0f7e70c907287697ecf2bcb31a088
Stefani <stefani.dewi@binus.ac.id>
2019-10-13 16:23:25 +0700

    Initial Commit

---
+++ Simulasi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simulasi
+//
+//  Created by Stefani Kurnia Permata Dewi on 13/10/19.
+//  Copyright © 2019 Stefani Kurnia Permata Dewi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
